535b746de424fa5279acad99e74661d17b5c8a8f
ahenches <57415229+ahenches@users.noreply.github.com>
2021-12-29 18:50:45 +0100

    implémentation de initialise_plateau, initialise_pioche
    modification de certains prototype de fonctions +initialise_plateau (implementation) + initialise_pioche(ajout implémentation)

--- triominos/modele.h
+++ triominos/modele.h
@@ -1,1 +1,33 @@
 // Bochra et Arnaud
+
+typedef struct triomino
+{
+    int min ;
+    int sec ;
+    int der ;
+
+} TRIOMINO;
+
+typedef struct emplacement
+{
+    TRIOMINO trio ;
+    char pointe ;
+} EMPLACEMENT;
+
+typedef struct main_joueur
+{
+    TRIOMINO tab[25];
+    int taille;
+} MAIN_JOUEUR;
+
+EMPLACEMENT ** initialise_plateau();
+TRIOMINO * initialise_pioche();
+// VOID distribution(nb_joueurs, tabJoueur);
+// JOUEUR joueur_qui_commence();
+void pioche(MAIN_JOUEUR main, TRIOMINO *pioche, int taillePioche);
+// VOID placer();
+// BOOL verifie_coup_valide(INT valeur1, INT valeur2, TRIOMINO a_placer);
+// INT verifie_figure();
+// BOOL test_fin();
+// INT calcule_score(JOUEUR j);
+// void jeu_ordinateur(JOUEUR ordi);
