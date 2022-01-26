30ba517b5486ed6e854c06f4558b3560132f9f9e
Moumsinho <rayane085@gmail.com>
2022-01-26 23:09:02 +0100

    update

--- vue.c
+++ vue.c
@@ -6,19 +6,24 @@ void affiche_menu_accueil()
 {
     POINT pGauche, pDroit;
 
-    pGauche.x = 0; pGauche.y = 900;
+    pGauche.x = 0;
+    pGauche.y = 900;
     affiche_image("./fond.bmp", pGauche);
 
-    pGauche.x = 500; pGauche.y = 700;
+    pGauche.x = 500;
+    pGauche.y = 700;
     affiche_texte("Bienvenue !", 50, pGauche, noir);
 
-    //Bouton Jouer
-    pGauche.x = 450 ; pGauche.y = 500;
-    pDroit.x = pGauche.x + 400 ; pDroit.y = pGauche.y - 200;
+    // Bouton Jouer
+    pGauche.x = 450;
+    pGauche.y = 500;
+    pDroit.x = pGauche.x + 400;
+    pDroit.y = pGauche.y - 200;
     dessine_rectangle_plein(pGauche, pDroit, lightgrey);
     dessine_rectangle(pGauche, pDroit, noir);
 
-    pGauche.x = 590; pGauche.y = 430;
+    pGauche.x = 590;
+    pGauche.y = 430;
     affiche_texte("Jouer", 40, pGauche, noir);
 }
 
@@ -27,18 +32,23 @@ void affiche_menu_nombre_joueurs()
     POINT pGauche, pDroit, pNombre;
     int i;
 
-    pGauche.x = 0; pGauche.y = 900;
+    pGauche.x = 0;
+    pGauche.y = 900;
     affiche_image("./fond.bmp", pGauche);
 
-    //Choix nombre joueurs humains
-    pGauche.x = 410 ; pGauche.y = 750;
+    // Choix nombre joueurs humains
+    pGauche.x = 410;
+    pGauche.y = 750;
     affiche_texte("Nombre joueurs humains : ", 40, pGauche, noir);
 
-        //Boutons de choix
-    pGauche.x = 520 ; pGauche.y = 630;
-    pDroit.x = pGauche.x + COTE_NB_JOUEUR ; pDroit.y = pGauche.y - COTE_NB_JOUEUR;
-    pNombre.x = pGauche.x + 15; pNombre.y = pGauche.y - 8;
-    for (i = 1 ; i <= 4 ; i++)
+    // Boutons de choix
+    pGauche.x = 520;
+    pGauche.y = 630;
+    pDroit.x = pGauche.x + COTE_NB_JOUEUR;
+    pDroit.y = pGauche.y - COTE_NB_JOUEUR;
+    pNombre.x = pGauche.x + 15;
+    pNombre.y = pGauche.y - 8;
+    for (i = 1; i <= 4; i++)
     {
         dessine_rectangle_plein(pGauche, pDroit, lightgrey);
         dessine_rectangle(pGauche, pDroit, noir);
@@ -49,15 +59,19 @@ void affiche_menu_nombre_joueurs()
         pNombre.x += COTE_NB_JOUEUR + 30;
     }
 
-    //Choix nombre de joueurs ordinateurs
-    pGauche.x = 400 ; pGauche.y = 500;
+    // Choix nombre de joueurs ordinateurs
+    pGauche.x = 400;
+    pGauche.y = 500;
     affiche_texte("Nombre joueurs ordinateurs : ", 40, pGauche, noir);
 
-        //Boutons choix
-    pGauche.x = 520 ; pGauche.y = 380;
-    pDroit.x = pGauche.x + COTE_NB_JOUEUR ; pDroit.y = pGauche.y - COTE_NB_JOUEUR;
-    pNombre.x = pGauche.x + 15; pNombre.y = pGauche.y - 8;
-    for (i = 0 ; i <= 3 ; i++)
+    // Boutons choix
+    pGauche.x = 520;
+    pGauche.y = 380;
+    pDroit.x = pGauche.x + COTE_NB_JOUEUR;
+    pDroit.y = pGauche.y - COTE_NB_JOUEUR;
+    pNombre.x = pGauche.x + 15;
+    pNombre.y = pGauche.y - 8;
+    for (i = 0; i <= 3; i++)
     {
         dessine_rectangle_plein(pGauche, pDroit, lightgrey);
         dessine_rectangle(pGauche, pDroit, noir);
@@ -68,13 +82,16 @@ void affiche_menu_nombre_joueurs()
         pNombre.x += COTE_NB_JOUEUR + 30;
     }
 
-    //Bouton Confirmer
-    pGauche.x = 480 ; pGauche.y = 230;
-    pDroit.x = pGauche.x + 400 ; pDroit.y = pGauche.y - 100 ;
+    // Bouton Confirmer
+    pGauche.x = 480;
+    pGauche.y = 230;
+    pDroit.x = pGauche.x + 400;
+    pDroit.y = pGauche.y - 100;
     dessine_rectangle_plein(pGauche, pDroit, lightgrey);
     dessine_rectangle(pGauche, pDroit, noir);
 
-    pGauche.x += 100; pGauche.y -= 25;
+    pGauche.x += 100;
+    pGauche.y -= 25;
     affiche_texte("Confirmer", 35, pGauche, noir);
 }
 
@@ -82,19 +99,23 @@ void affiche_menu_pseudo(int numeroJoueur)
 {
     POINT pGauche;
 
-    pGauche.x = 0; pGauche.y = 900;
+    pGauche.x = 0;
+    pGauche.y = 900;
     affiche_image("./fond.bmp", pGauche);
 
-    //Affichage du joueur qui doit entrer son pseudo
-    pGauche.x = 530; pGauche.y = 600 ;
+    // Affichage du joueur qui doit entrer son pseudo
+    pGauche.x = 530;
+    pGauche.y = 600;
     affiche_texte("Joueur ", 50, pGauche, noir);
     pGauche.x += 200;
     affiche_entier(numeroJoueur, 50, pGauche, noir);
 
-    pGauche.x = 450; pGauche.y = 480;
+    pGauche.x = 450;
+    pGauche.y = 480;
     affiche_texte("Entrez votre pseudo", 40, pGauche, noir);
 
-    pGauche.x = 500; pGauche.y = 400;
+    pGauche.x = 500;
+    pGauche.y = 400;
     affiche_texte("Dans la console", 35, pGauche, noir);
 }
 
@@ -102,26 +123,33 @@ void affiche_menu_choix_jeu()
 {
     POINT pGauche, pDroit, pTexte;
 
-    pGauche.x = 0; pGauche.y = 900;
+    pGauche.x = 0;
+    pGauche.y = 900;
     affiche_image("./fond.bmp", pGauche);
 
-    pGauche.x = 440; pGauche.y = 750;
+    pGauche.x = 440;
+    pGauche.y = 750;
     affiche_texte("A quoi voulez vous jouer ?", 35, pGauche, noir);
 
-    pGauche.x = 650; pGauche.y = 650;
-    pDroit.x = 650; pDroit.y = 250;
-    dessine_ligne(pGauche,pDroit, noir);
+    pGauche.x = 650;
+    pGauche.y = 650;
+    pDroit.x = 650;
+    pDroit.y = 250;
+    dessine_ligne(pGauche, pDroit, noir);
 
-    //Bouton Dominos
-    pGauche.x = 125; pGauche.y = 580;
-    pDroit.x = pGauche.x + 400; pDroit.y = pGauche.y - 100;
+    // Bouton Dominos
+    pGauche.x = 125;
+    pGauche.y = 580;
+    pDroit.x = pGauche.x + 400;
+    pDroit.y = pGauche.y - 100;
     dessine_rectangle_plein(pGauche, pDroit, lightgrey);
     dessine_rectangle(pGauche, pDroit, noir);
 
-    pTexte.x = pGauche.x + 110; pTexte.y = pGauche.y - 25;
+    pTexte.x = pGauche.x + 110;
+    pTexte.y = pGauche.y - 25;
     affiche_texte("Dominos", 35, pTexte, noir);
 
-    //Bouton Triominos
+    // Bouton Triominos
     pGauche.x += 650;
     pDroit.x = pGauche.x + 400;
     dessine_rectangle_plein(pGauche, pDroit, lightgrey);
@@ -130,17 +158,20 @@ void affiche_menu_choix_jeu()
     pTexte.x = pGauche.x + 110;
     affiche_texte("Triominos", 35, pTexte, noir);
 
-    //Boutons choix Dominos
-        //Avec Pioche
-    pGauche.x = 155; pGauche.y = 350;
-    pDroit.x = pGauche.x + 150; pDroit.y = pGauche.y - 50;
+    // Boutons choix Dominos
+    // Avec Pioche
+    pGauche.x = 155;
+    pGauche.y = 350;
+    pDroit.x = pGauche.x + 150;
+    pDroit.y = pGauche.y - 50;
     dessine_rectangle_plein(pGauche, pDroit, lightgrey);
     dessine_rectangle(pGauche, pDroit, noir);
 
-    pTexte.x = pGauche.x + 20; pTexte.y = pGauche.y - 10;
+    pTexte.x = pGauche.x + 20;
+    pTexte.y = pGauche.y - 10;
     affiche_texte("Avec Pioche", 20, pTexte, noir);
 
-        //Sans Pioche
+    // Sans Pioche
     pGauche.x += 190;
     pDroit.x = pGauche.x + 150;
     dessine_rectangle_plein(pGauche, pDroit, lightgrey);
@@ -149,8 +180,8 @@ void affiche_menu_choix_jeu()
     pTexte.x = pGauche.x + 20;
     affiche_texte("Sans Pioche", 20, pTexte, noir);
 
-    //Boutons choix Triominos
-        //Avec Score
+    // Boutons choix Triominos
+    // Avec Score
     pGauche.x += 460;
     pDroit.x = pGauche.x + 150;
     dessine_rectangle_plein(pGauche, pDroit, lightgrey);
@@ -159,7 +190,7 @@ void affiche_menu_choix_jeu()
     pTexte.x = pGauche.x + 20;
     affiche_texte("Avec Score", 20, pTexte, noir);
 
-        //Sans Score
+    // Sans Score
     pGauche.x += 190;
     pDroit.x = pGauche.x + 150;
     dessine_rectangle_plein(pGauche, pDroit, lightgrey);
@@ -168,41 +199,49 @@ void affiche_menu_choix_jeu()
     pTexte.x = pGauche.x + 20;
     affiche_texte("Sans Score", 20, pTexte, noir);
 
-    //Bouton jouer
-    pGauche.x = 500; pGauche.y = 200;
-    pDroit.x = pGauche.x + 300; pDroit.y = pGauche.y - 100;
+    // Bouton jouer
+    pGauche.x = 500;
+    pGauche.y = 200;
+    pDroit.x = pGauche.x + 300;
+    pDroit.y = pGauche.y - 100;
     dessine_rectangle_plein(pGauche, pDroit, lightgrey);
     dessine_rectangle(pGauche, pDroit, noir);
 
-    pTexte.x = pGauche.x + 110; pTexte.y = pGauche.y - 30;
+    pTexte.x = pGauche.x + 110;
+    pTexte.y = pGauche.y - 30;
     affiche_texte("Jouer", 30, pTexte, noir);
 }
 
 void affiche_menu_classement()
 {
     POINT pGauche, pDroit, pTexte;
-    FILE* fichier = NULL;
+    FILE *fichier = NULL;
     char texte[500] = "";
 
-    pGauche.x = 0; pGauche.y = 900;
+    pGauche.x = 0;
+    pGauche.y = 900;
     affiche_image("./fond.bmp", pGauche);
 
-    pTexte.x = 510; pTexte.y = 750;
+    pTexte.x = 510;
+    pTexte.y = 750;
     affiche_texte("Classement", 40, pTexte, noir);
 
     fichier = fopen("./classement.txt", "r");
 
-    //Tableau du classement
-    pGauche.x = 450; pGauche.y = 650 ;
-    pDroit.x = pGauche.x +400 ; pDroit.y = pGauche.y -100 ;
-    pTexte.x = pGauche.x +30 ;pTexte.y = pGauche.y -40 ;
+    // Tableau du classement
+    pGauche.x = 450;
+    pGauche.y = 650;
+    pDroit.x = pGauche.x + 400;
+    pDroit.y = pGauche.y - 100;
+    pTexte.x = pGauche.x + 30;
+    pTexte.y = pGauche.y - 40;
     if (fichier != NULL)
     {
-        while(fgets(texte, 500, fichier) != NULL)
+        while (fgets(texte, 500, fichier) != NULL)
         {
-            dessine_rectangle_plein(pGauche,pDroit,bleuclair);
-            dessine_rectangle(pGauche,pDroit, noir);
-            affiche_texte( texte, 20 , pTexte, noir );
+            dessine_rectangle_plein(pGauche, pDroit, bleuclair);
+            dessine_rectangle(pGauche, pDroit, noir);
+            affiche_texte(texte, 20, pTexte, noir);
             pGauche.y -= 100;
             pDroit.y -= 100;
             pTexte.y -= 100;
@@ -210,34 +249,39 @@ void affiche_menu_classement()
         fclose(fichier);
     }
 
-    //Boutons Rejouer et Quitter
-    pGauche.x = 425; pGauche.y = 200 ;
-    pDroit.x = pGauche.x +200 ; pDroit.y = pGauche.y -100 ;
-    pTexte.x = pGauche.x + 40 ;pTexte.y = pGauche.y -30 ;
+    // Boutons Rejouer et Quitter
+    pGauche.x = 425;
+    pGauche.y = 200;
+    pDroit.x = pGauche.x + 200;
+    pDroit.y = pGauche.y - 100;
+    pTexte.x = pGauche.x + 40;
+    pTexte.y = pGauche.y - 30;
 
-    dessine_rectangle_plein(pGauche,pDroit,lightgrey);
-    dessine_rectangle(pGauche,pDroit, noir);
-    affiche_texte("Rejouer" , 30 , pTexte, noir );
+    dessine_rectangle_plein(pGauche, pDroit, lightgrey);
+    dessine_rectangle(pGauche, pDroit, noir);
+    affiche_texte("Rejouer", 30, pTexte, noir);
 
     pGauche.x += 250;
     pDroit.x += 250;
     pTexte.x += 250;
 
-    dessine_rectangle_plein(pGauche,pDroit,lightgrey);
-    dessine_rectangle(pGauche,pDroit, noir);
-    affiche_texte("Quitter" , 30 , pTexte, noir );
+    dessine_rectangle_plein(pGauche, pDroit, lightgrey);
+    dessine_rectangle(pGauche, pDroit, noir);
+    affiche_texte("Quitter", 30, pTexte, noir);
 }
 
 void affiche_selection_nb_joueurs_humains(int nbHumains)
 {
     POINT pGauche, pDroit;
 
-    //Met en evidence le nombre selectionné, efface la selection precedente
-    pGauche.x = 520 ; pGauche.y = 630;
-    pDroit.x = pGauche.x + COTE_NB_JOUEUR ; pDroit.y = pGauche.y - COTE_NB_JOUEUR;
-    for (int i = 1 ; i <= 4 ; i++)
+    // Met en evidence le nombre selectionné, efface la selection precedente
+    pGauche.x = 520;
+    pGauche.y = 630;
+    pDroit.x = pGauche.x + COTE_NB_JOUEUR;
+    pDroit.y = pGauche.y - COTE_NB_JOUEUR;
+    for (int i = 1; i <= 4; i++)
     {
-        if(i == nbHumains)
+        if (i == nbHumains)
             dessine_rectangle(pGauche, pDroit, rouge);
         else
             dessine_rectangle(pGauche, pDroit, noir);
@@ -250,12 +294,14 @@ void affiche_selection_nb_joueurs_ordi(int nbOrdi)
 {
     POINT pGauche, pDroit;
 
-    //Met en evidence le nombre selectionné, efface la selection precedente
-    pGauche.x = 520 ; pGauche.y = 380;
-    pDroit.x = pGauche.x + COTE_NB_JOUEUR ; pDroit.y = pGauche.y - COTE_NB_JOUEUR;
-    for (int i = 0; i <= 3 ; i++)
+    // Met en evidence le nombre selectionné, efface la selection precedente
+    pGauche.x = 520;
+    pGauche.y = 380;
+    pDroit.x = pGauche.x + COTE_NB_JOUEUR;
+    pDroit.y = pGauche.y - COTE_NB_JOUEUR;
+    for (int i = 0; i <= 3; i++)
     {
-        if(i == nbOrdi)
+        if (i == nbOrdi)
             dessine_rectangle(pGauche, pDroit, rouge);
         else
             dessine_rectangle(pGauche, pDroit, noir);
@@ -267,11 +313,13 @@ void affiche_selection_nb_joueurs_ordi(int nbOrdi)
 
 void affiche_selection_jeu(JEU jeuSelect)
 {
-    POINT pGauche,  pDroit;
+    POINT pGauche, pDroit;
 
-    //Efface la selection precedente des variantes
-    pGauche.x = 155; pGauche.y = 350;
-    pDroit.x = pGauche.x + 150; pDroit.y = pGauche.y - 50;
+    // Efface la selection precedente des variantes
+    pGauche.x = 155;
+    pGauche.y = 350;
+    pDroit.x = pGauche.x + 150;
+    pDroit.y = pGauche.y - 50;
     dessine_rectangle(pGauche, pDroit, noir);
 
     pGauche.x += 190;
@@ -287,22 +335,23 @@ void affiche_selection_jeu(JEU jeuSelect)
     dessine_rectangle(pGauche, pDroit, noir);
 
     // affiche la selection du jeu
-    pGauche.x = 125; pGauche.y = 580;
-    pDroit.x = pGauche.x + 400; pDroit.y = pGauche.y - 100;
-    if(jeuSelect == DOMINOS)
+    pGauche.x = 125;
+    pGauche.y = 580;
+    pDroit.x = pGauche.x + 400;
+    pDroit.y = pGauche.y - 100;
+    if (jeuSelect == DOMINOS)
     {
         dessine_rectangle(pGauche, pDroit, rouge);
         pGauche.x += 650;
         pDroit.x = pGauche.x + 400;
         dessine_rectangle(pGauche, pDroit, noir);
-
     }
-    else{
+    else
+    {
         dessine_rectangle(pGauche, pDroit, noir);
         pGauche.x += 650;
         pDroit.x = pGauche.x + 400;
         dessine_rectangle(pGauche, pDroit, rouge);
-
     }
 }
 
@@ -310,9 +359,11 @@ void affiche_selection_variante(VARIANTE varSelect)
 {
     POINT pGauche, pDroit;
 
-    //Efface la selection precedente
-    pGauche.x = 155; pGauche.y = 350;
-    pDroit.x = pGauche.x + 150; pDroit.y = pGauche.y - 50;
+    // Efface la selection precedente
+    pGauche.x = 155;
+    pGauche.y = 350;
+    pDroit.x = pGauche.x + 150;
+    pDroit.y = pGauche.y - 50;
     dessine_rectangle(pGauche, pDroit, noir);
 
     pGauche.x += 190;
@@ -327,31 +378,39 @@ void affiche_selection_variante(VARIANTE varSelect)
     pDroit.x = pGauche.x + 150;
     dessine_rectangle(pGauche, pDroit, noir);
 
-    //Affiche la nouvelle selection
-    switch(varSelect)
+    // Affiche la nouvelle selection
+    switch (varSelect)
     {
-        case SANS_PIOCHE :
-            affiche_selection_jeu(DOMINOS);
-            pGauche.x = 345; pGauche.y = 350;
-            pDroit.x = pGauche.x + 150; pDroit.y = pGauche.y - 50;
-            dessine_rectangle(pGauche, pDroit, rouge);
-            break;
-        case AVEC_PIOCHE :
-            affiche_selection_jeu(DOMINOS);
-            pGauche.x = 155; pGauche.y = 350;
-            pDroit.x = pGauche.x + 150; pDroit.y = pGauche.y - 50;
-            dessine_rectangle(pGauche, pDroit, rouge);
-            break;
-        case AVEC_SCORE :
-            affiche_selection_jeu(TRIOMINOS);
-            pGauche.x = 805; pGauche.y = 350;
-            pDroit.x = pGauche.x + 150; pDroit.y = pGauche.y - 50;
-            dessine_rectangle(pGauche, pDroit, rouge);
-            break;
-         default :
-             affiche_selection_jeu(TRIOMINOS);
-             pGauche.x = 995; pGauche.y = 350;
-             pDroit.x = pGauche.x + 150; pDroit.y = pGauche.y - 50;
-             dessine_rectangle(pGauche, pDroit, rouge);
-     }
+    case SANS_PIOCHE:
+        affiche_selection_jeu(DOMINOS);
+        pGauche.x = 345;
+        pGauche.y = 350;
+        pDroit.x = pGauche.x + 150;
+        pDroit.y = pGauche.y - 50;
+        dessine_rectangle(pGauche, pDroit, rouge);
+        break;
+    case AVEC_PIOCHE:
+        affiche_selection_jeu(DOMINOS);
+        pGauche.x = 155;
+        pGauche.y = 350;
+        pDroit.x = pGauche.x + 150;
+        pDroit.y = pGauche.y - 50;
+        dessine_rectangle(pGauche, pDroit, rouge);
+        break;
+    case AVEC_SCORE:
+        affiche_selection_jeu(TRIOMINOS);
+        pGauche.x = 805;
+        pGauche.y = 350;
+        pDroit.x = pGauche.x + 150;
+        pDroit.y = pGauche.y - 50;
+        dessine_rectangle(pGauche, pDroit, rouge);
+        break;
+    default:
+        affiche_selection_jeu(TRIOMINOS);
+        pGauche.x = 995;
+        pGauche.y = 350;
+        pDroit.x = pGauche.x + 150;
+        pDroit.y = pGauche.y - 50;
+        dessine_rectangle(pGauche, pDroit, rouge);
+    }
 }
