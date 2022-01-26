8c08ab4ff002be5d7960a66c35d8d24c3563678a
ahenches <57415229+ahenches@users.noreply.github.com>
2022-01-26 02:14:26 +0100

    Revert "ajout test hexagone bochra"
    This reverts commit 7094255090ba5099ce0c4a7e883feae3463f46bb.

--- triominos/modele.h
+++ triominos/modele.h
@@ -5,13 +5,6 @@ typedef struct nb_joueur
     int nbJouOrdi;
 } NB_JOUEUR;
 
-typedef struct coup
-{
-  int indice_trio_dans_main;
-  int indice_ligne;
-  int indice_colonne;
-} COUP; // utiliser dans la fonction jeu_ordinateur
-
 EMPLACEMENT ** initialise_plateau();
 PIOCHE_TRIOMINOS initialise_pioche();
 void initialise_ordis(NB_JOUEUR nb_joueur, JOUEUR_TRIOMINOS *joueurs);
@@ -21,9 +14,9 @@ JOUEUR_TRIOMINOS joueur_qui_commence(NB_JOUEUR nb_joueur,
   JOUEUR_TRIOMINOS *joueurs, PIOCHE_TRIOMINOS *pioche_initiale);
 
 void pioche(MAIN_J_TRIOMINOS *main, PIOCHE_TRIOMINOS *pioche);
-int placer_trio(TRIOMINO TrioAPlacer, EMPLACEMENT **tabEmpl, int c, int l);
+void placer_trio(TRIOMINO TrioAPlacer, EMPLACEMENT **tabEmpl, int x, int y);
 BOOL verif_coup_valide(int v1, int v2, TRIOMINO trio);
 // INT verifie_figure();
 // BOOL test_fin();
 // INT calcule_score(JOUEUR j);
-int jeu_ordinateur(JOUEUR_TRIOMINOS ordi, EMPLACEMENT **tabEmplacement);
+void jeu_ordinateur(JOUEUR_TRIOMINOS ordi, EMPLACEMENT **tabEmplacement);
